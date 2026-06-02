7856172221af8948f0b6df780c832c6b309e7965
HoangDuc1307 <duc123123123slsls@gmail.com>
2026-06-02 14:59:13 +0700

    Cap nhat giao dien sang, loai bo favicon va toi uu hoa hien thi

--- pothole_project/urls.py
+++ pothole_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 from django.conf import settings
 
